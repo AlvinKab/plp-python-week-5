086f11a34b575c41df504b492d303f7e91562977
AlvinKab <alkmwas@gmail.com>
2024-09-09 21:26:41 +0300

    added exception handling

--- file_handling_assignment.py
+++ file_handling_assignment.py
@@ -1,8 +1,13 @@
-text_file = open("my_file.txt", "a+")
-text_file.write("\nMore lines!!!\n")
-text_file.write("Line 7\n")
-text_file.write("Line 8\n")
-text_file.write("Line 9")
-text_file.seek(0)
-file_content = text_file.read()
-print(file_content)
+try:
+    text_file = open("my_file.txt", "a+")
+    text_file.write("\nMore lines!!!\n")
+    text_file.write("Line 10\n")
+    text_file.write("Line 11\n")
+    text_file.write("Line 12")
+    text_file.seek(0)
+    file_content = text_file.read()
+    print(file_content)
+except:
+    print("An error occurred")
+finally:
+    print("Session terminated")
